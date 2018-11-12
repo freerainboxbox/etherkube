7b2ada197e803d66c6084b2d042ed859ee10c1d2
freerainboxbox <linmichael02@gmail.com>
2018-11-12 13:18:19 -0500

    Changed Geth start.sh
    Changed network to devnet and set block time to 15 sec (CT_sub_b of Ethereum in Experiment Outline)

--- containers/geth-stable/start.sh
+++ containers/geth-stable/start.sh
@@ -3,9 +3,9 @@
 echo "-------------------------------------------------------"
 echo "Run Geth stable binary"
 echo "-------------------------------------------------------"
-
+#Points to PoA devnet and sets block time to 15
 if [ -z "$NODE_OPTIONS" ]; then
-    NODE_OPTIONS="--cache 4096"
+    NODE_OPTIONS="--cache 4096 --dev --dev.period 15"
 fi
 
 /opt/geth/geth \
